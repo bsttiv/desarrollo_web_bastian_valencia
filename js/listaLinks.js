1db5893bdccee5e2c2c1eb51edcea1eea66fc61c
bsttiv <bastian.valencia@ug.uchile.cl>
2025-09-01 20:46:37 -0400

    Arreglar errores con las rutas

--- js/listaLinks.js
+++ js/listaLinks.js
@@ -2,6 +2,6 @@ let tableRows = document.getElementsByClassName("aviso")
 for (const row of tableRows){
     row.onclick = () => {
         console.log("test")
-        window.location = "/avisos/"+row.id+".html"
+        window.location = "avisos/"+row.id+".html"
     }
 }
